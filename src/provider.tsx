2d88a2173aa1c64dca09cce30f5b348817318b3a
Marcos Gonçalves <contact@themgoncalves.com>
2023-01-03 20:35:43 +0100

    fix cache implementation

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,5 +1,5 @@
-import { createEffect, createContext, createSignal } from "solid-js";
-import { createStore } from "solid-js/store";
+import { createEffect, createContext } from "solid-js";
+import { createStore, createMutable } from "solid-js/store";
 import { IntlCache, createIntl, createIntlCache } from "@formatjs/intl";
 import type { IntlShape } from "@formatjs/intl";
 import type { FlowComponent } from "solid-js";
@@ -10,11 +10,11 @@ import type { IntlConfig } from "./types";
 const IntlContext = createContext<IntlShape>();
 
 const IntlProvider: FlowComponent<IntlConfig> = (props) => {
-  const [cache] = createSignal<IntlCache>(createIntlCache());
-  const [intl, setIntl] = createStore(createIntl(processConfig(props), cache()));
+  const cache = createMutable<IntlCache>(createIntlCache());
+  const [intl, setIntl] = createStore(createIntl(processConfig(props), cache));
 
   createEffect(() => {
-    setIntl(createIntl(processConfig(props), cache()));
+    setIntl(createIntl(processConfig(props), cache));
   });
 
   return <IntlContext.Provider value={intl}>{props.children}</IntlContext.Provider>;
