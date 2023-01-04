d2ffed7a47c93010d115ab68462c6d4736491f66
Marcos Gonçalves <contact@themgoncalves.com>
2023-01-04 16:01:53 +0100

    fix package build

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,5 +2,14 @@ import withSolid from 'rollup-preset-solid';
 
 export default withSolid({
   input: 'src/index.ts',
-  targets: ['esm', 'cjs'],
+  output: [
+    {
+      file: "dist/cjs/index.cjs",
+      format: "cjs"
+    },
+    {
+      file: "dist/esm/index.js",
+      format: "esm"
+    }
+  ],
 });
